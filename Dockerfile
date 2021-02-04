605b1bc098d1059b0778b556d06cd6f3159a7d5c
Michael Dwan <m@dwan.io>
2021-02-03 17:50:32 -0700

    prom exporter

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,8 @@ RUN CGO_ENABLED=0 GOOS=linux go build -v -o flycheck ./cmd/flycheck
 
 FROM flyio/stolon:20210109-5 as stolon
 
+FROM wrouesnel/postgres_exporter:latest AS postgres_exporter
+
 FROM postgres:12.5
 
 LABEL fly.app_role=postgres_cluster
@@ -17,6 +19,7 @@ RUN apt-get update && apt-get install --no-install-recommends -y \
     && apt autoremove -y
 
 COPY --from=stolon /go/src/app/bin/* /usr/local/bin/
+COPY --from=postgres_exporter /postgres_exporter /usr/local/bin/
 ADD /bin/* /usr/local/bin/
 ADD /scripts/* /fly/
 RUN useradd -ms /bin/bash stolon
--- scripts/start.sh
+++ scripts/start.sh
@@ -20,6 +20,8 @@ fi
 
 consul_node="$(echo $FLY_CONSUL_URL | sed -r 's/https:\/\/[^\/]+\///')$node_id"
 
+mkdir -p /data
+
 # because stolon needs entirely different sets of env vars :/
 cat <<EOF > /data/.env
 STOLONCTL_CLUSTER_NAME=$FLY_APP_NAME
@@ -45,7 +47,7 @@ primary_region="${PRIMARY_REGION}"
 pg_proxy_port="${PG_PROXY_PORT:-5432}"
 pg_port="${PG_PORT:-5433}"
 
-keeper_options="--uid $node_id --data-dir /data/ --pg-su-username=flypgadmin --pg-repl-username=repluser --pg-listen-address=$ip --pg-port $pg_port --log-level warn"
+keeper_options="--uid $node_id --data-dir /data/ --pg-su-username=flypgadmin --pg-repl-username=repluser --pg-listen-address=$ip --pg-port $pg_port --log-level warn --metrics-listen-address 0.0.0.0:8080"
 
 if [ "$primary_region" != "" ]; then
     if [ "$primary_region" != "$FLY_REGION" ]; then
@@ -62,6 +64,7 @@ cat << EOF > /fly/Procfile
 keeper: stolon-keeper $keeper_options
 sentinel: stolon-sentinel --initial-cluster-spec /fly/initial-cluster-spec.json
 proxy: stolon-proxy --listen-address=$ip --port=$pg_proxy_port --log-level=warn
+postgres_exporter: DATA_SOURCE_URI=[$ip]:$pg_port/postgres?sslmode=disable DATA_SOURCE_PASS=$SU_PASSWORD DATA_SOURCE_USER=flypgadmin PG_EXPORTER_DISABLE_SETTINGS_METRICS=true postgres_exporter
 EOF
 
 chown -R stolon:stolon /data/
