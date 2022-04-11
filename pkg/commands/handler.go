de79341d69f1a6714540db8199f2d92aa4dffa82
Rugwiro Mbanda Valentin <rugwirobaker@gmail.com>
2022-04-11 16:37:54 +0200

    Implement failover over http

--- pkg/commands/handler.go
+++ pkg/commands/handler.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"time"
 
 	"github.com/fly-examples/postgres-ha/pkg/flypg"
 	"github.com/fly-examples/postgres-ha/pkg/flypg/admin"
+	"github.com/fly-examples/postgres-ha/pkg/flypg/stolon"
 	"github.com/fly-examples/postgres-ha/pkg/render"
+	"github.com/fly-examples/postgres-ha/pkg/util"
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v4"
 )
@@ -257,9 +260,70 @@ func getConnection(ctx context.Context) (*pgx.Conn, func() error, error) {
 }
 
 func handleFailover(w http.ResponseWriter, r *http.Request) {
-	res := &Response{Result: true}
+	ctx := r.Context()
 
-	render.JSON(w, res, http.StatusOK)
+	env, err := util.BuildEnv()
+	if err != nil {
+		render.Err(w, err)
+		return
+	}
+
+	data, err := stolon.FetchClusterData(env)
+	if err != nil {
+		render.Err(w, err)
+		return
+	}
+
+	// Set this so we can compare it later.
+	currentMasterUID := masterKeeperUID(data)
+
+	// Discover keepers that are eligible for promotion.
+	eligibleCount := 0
+	for _, keeper := range data.Keepers {
+		if keeper.Status.Healthy && keeper.Status.CanBeMaster != nil && keeper.UID != currentMasterUID {
+			fmt.Printf("Keeper %s is eligible!  Master is %s\n", keeper.UID, currentMasterUID)
+			eligibleCount++
+		}
+	}
+
+	if eligibleCount == 0 {
+		err := fmt.Errorf("no eligible keepers available to accommodate failover")
+		render.Err(w, err)
+		return
+	}
+
+	_, err = stolon.Failkeeper(currentMasterUID, env)
+	if err != nil {
+		util.WriteError(err)
+	}
+
+	// Verify failover
+	timeout := time.After(10 * time.Second)
+
+	ticker := time.NewTicker(1 * time.Second)
+
+	for {
+		select {
+
+		case <-timeout:
+			render.Err(w, fmt.Errorf("timed out verifying failover"))
+		case <-ticker.C:
+			data, err := stolon.FetchClusterData(env)
+			if err != nil {
+				render.Err(w, fmt.Errorf("failed to verify failover with error: %w", err))
+			}
+
+			if currentMasterUID != masterKeeperUID(data) {
+				res := failOverResponse{"failover completed successfully"}
+				render.JSON(w, res, http.StatusOK)
+				return
+			}
+		case <-ctx.Done():
+			render.Err(w, ctx.Err())
+			return
+		}
+
+	}
 }
 
 func handleRestart(w http.ResponseWriter, r *http.Request) {
@@ -307,4 +371,9 @@ func handleRole(w http.ResponseWriter, r *http.Request) {
 	res := &Response{Result: role}
 
 	render.JSON(w, res, http.StatusOK)
+}
+
+func masterKeeperUID(data *stolon.ClusterData) string {
+	db := data.DBs[data.Cluster.Status.Master]
+	return db.Spec.KeeperUID
 }
