e2877365d003e505868d4f7e4364493993894334
Pavel Borzenkov <pavel@borzenkov.net>
2023-04-18 10:09:03 +0200

    Fix migration to Machines for databases with hyphens

--- pkg/flypg/admin/admin.go
+++ pkg/flypg/admin/admin.go
@@ -297,7 +297,7 @@ func SetReadonly(ctx context.Context, pg *pgx.Conn, enable bool) error {
 			continue
 		}
 
-		sql := fmt.Sprintf("ALTER DATABASE %s SET default_transaction_read_only=%v;", db.Name, enable)
+		sql := fmt.Sprintf("ALTER DATABASE %q SET default_transaction_read_only=%v;", db.Name, enable)
 		if _, err = pg.Exec(ctx, sql); err != nil {
 			return fmt.Errorf("failed to alter readonly state on db %s: %s", db.Name, err)
 		}
